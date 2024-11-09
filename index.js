86cb5e99dbc0a8cf5c13b8fd392c04afe7a30946
Tran Quoc Khang <tranquockhang1810@gmail.com>
2024-11-09 15:03:04 +0700

    API get products

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ const app = express();
 const port = 8000;
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/website', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27019/', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('Error connecting to MongoDB:', error));
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const Product = mongoose.model('Product', {
+  name: String,
+  price: Number,
 });
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.get('/api/v1/products', (req, res) => {
+  Product.find()
+    .then(products => res.json({
+      status: 'success',
+      data: products
+    }))
+    .catch(error => res.status(500).json({ error }));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
